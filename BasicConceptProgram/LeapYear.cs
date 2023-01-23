92cdd70ccc5489ce9d4eaa2305cfd4e36b64f0a3
Snehal979 <bansodsnehal535@gmail.com>
2023-01-23 21:28:44 +0530

    [Snehal]Add: Check the Year is Leap year or Not Leap year

---
+++ BasicConceptProgram/LeapYear.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConceptProgram
+{
+    internal class LeapYear
+    {
+        public void CheckTheLeapYear()
+        {
+            Console.WriteLine("Enter the Year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            //Ensure that the year is Four Digit
+            if (year.ToString().Length == 4)
+            {
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine("{0} is the Leap Year", year);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not Leap Year", year);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ensure that the year is Four digit");
+            }
+        }
+                 
+    }
+}
--- BasicConceptProgram/Program.cs
+++ BasicConceptProgram/Program.cs
@@ -7,13 +7,17 @@
             try
             {
                 Console.WriteLine("Basic Concept Program");
-                Console.WriteLine("Hint 1.FlipCoin");
+                Console.WriteLine("Hint 1.FlipCoin 2.Leap Year");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         FilpCoinParcentage filpCoinParcentage = new FilpCoinParcentage();
                         filpCoinParcentage.CalculatePercentageOfFilpCoin();
+                        break;
+                    case 2:
+                        LeapYear leapYear = new LeapYear();
+                        leapYear.CheckTheLeapYear();
                         break;
                 }
             }
